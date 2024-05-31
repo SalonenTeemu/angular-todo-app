e1750005240cc158598fdaf9d7dd16a9020234b2
Teemu Salonen <teemu.t.salonen@tuni.fi>
2024-05-31 13:59:58 +0300

    Added todo list, item deletion and styling

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,22 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { TodoService } from '../shared/todo.service';
-import { Observable, map } from 'rxjs';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-todo',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './todo.component.html',
   styles: [],
 })
 export class TodoComponent implements OnInit {
   todos$: Observable<any[]> | undefined;
+  todoList: any[] = [];
 
   constructor(private todoService: TodoService) {}
 
   ngOnInit(): void {
-    this.todos$ = this.todoService
-      .getTodos()
-      .pipe(map((todos) => todos.sort((a, b) => a.completed - b.completed)));
+    this.todos$ = this.todoService.getTodos();
+    this.todos$.subscribe((data) => {
+      this.todoList = data.sort((a: any, b: any) => {
+        // Sort by completion status (true first, false last)
+        if (a.completed === b.completed) {
+          // If completion status is the same, sort alphabetically by title
+          return a.title.localeCompare(b.title);
+        }
+        return a.completed ? -1 : 1; // Sort completed items first
+      });
+    });
   }
 
   onClick(titleInput: HTMLInputElement) {
